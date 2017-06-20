302619cac6808cbbb1f43f44f8b2b8b1572de29a
sukhi3101 <sukhi3101@users.noreply.github.com>
2017-06-20 08:47:55 -0700

    <Assignment5>

---
+++ src/assignment1/Bicycle.java
@@ -0,0 +1,21 @@
+package assignment1;
+
+public class Bicycle  implements Vehicle{
+	public void setNoOfTires(int number){
+		System.out.println("Vehicle Type: Bicycle");
+		System.out.println("Num-tires: "+number);
+	}
+	public void setWeight(int pounds){
+		System.out.println("Weight: "+pounds);
+	}
+	public void setMake(String make){
+		System.out.println("Make: "+make);
+			
+	}
+	public void setModel(String model){
+		System.out.println("Model: "+model);
+		
+	}
+
+
+}
