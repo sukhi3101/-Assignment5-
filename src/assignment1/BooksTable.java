c75b9cbd81dd2d93d5e789cf4c201fca0ed970ed
sukhi3101 <sukhi3101@users.noreply.github.com>
2017-06-20 08:55:47 -0700

    <Assignment5>

---
+++ src/assignment1/BooksTable.java
@@ -0,0 +1,17 @@
+package assignment1;
+
+public class BooksTable {
+
+	public static void main(String[] args) {
+				System.out.println("column ID\tauthor\tbookname\tcategory");
+				int bookarray[]={};
+				
+				for(int a=1;a<bookarray.length;a++) {
+					System.out.println(a + "\t" + bookarray[a]);
+				}
+		}
+		
+
+	}
+
+
